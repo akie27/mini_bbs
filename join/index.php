18197b143ea09b05e3384a3b2b8bdb5106c5ade6
akie27 <66990408+akie27@users.noreply.github.com>
2021-11-29 13:49:07 +0900

    first commit

--- join/index.php
+++ join/index.php
@@ -66,56 +66,56 @@ if($_REQUEST['action'] == 'rewrite'){
 </head>
 
 <body>
-<div id="wrap">
-  <div id="head">
-    <h1>ひとこと掲示板</h1>
-  </div>
-  <div id="content">
-    <p>次のフォームに必要事項をご記入ください。</p>
-    <form action="" method="post" enctype="multipart/form-data">
-      <dl>
-        <dt>ニックネーム<span class="required">必須</span></dt>
-        <dd>
-          <input type="text" name="name" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['name'], ENT_QUOTES); ?>">
-          <?php if($error['name'] == 'blank'): ?>
-          <p class="error">* ニックネームを入力してください</p>
-          <?php endif; ?>
-        </dd>
-        <dt>メールアドレス<span class="required">必須</span></dt>
-        <dd>
-          <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES); ?>">
-          <?php if($error['email'] == 'blank'): ?>
-          <p class="error">* メールアドレスを入力してください</p>
-          <?php endif; ?>
-          <?php if($error['email'] == 'duplicate'): ?>
-            <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
+  <div id="wrap">
+    <div id="head">
+      <h1>ひとこと掲示板</h1>
+    </div>
+    <div id="content">
+      <p>次のフォームに必要事項をご記入ください。</p>
+      <form action="" method="post" enctype="multipart/form-data">
+        <dl>
+          <dt>ニックネーム<span class="required">必須</span></dt>
+          <dd>
+            <input type="text" name="name" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['name'], ENT_QUOTES); ?>">
+            <?php if($error['name'] == 'blank'): ?>
+            <p class="error">* ニックネームを入力してください</p>
             <?php endif; ?>
-        </dd>
-        <dt>パスワード<span class="required">必須</span></dt>
-        <dd>
-          <input type="password" name="password" size="10" maxlength="20" value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES); ?>">
-          <?php if($error['password'] == 'blank'): ?>
-          <p class="error">* パスワードを入力してください</p>
-          <?php endif; ?>
-          <?php if($error['password'] == 'length'): ?>
-          <p class="error">* パスワードは4文字以上で入力してください</p>
-          <?php endif; ?>
-        </dd>
-        <dt>写真など</dt>
-        <dd>
-          <input type="file" name="image" size="35">
-          <?php if($error['image'] == 'type'): ?>
-          <p class="error">* 写真などは「.gif」または「.jpg」の画像を指定してください</p>
-          <?php endif; ?>
-          <?php if(!empty($error)): ?>
-          <p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
-          <?php endif; ?>
-        </dd>
-      </dl>
-      <div><input type="submit" value="入力内容を確認する"></div>
-    </form>
-  </div>
+          </dd>
+          <dt>メールアドレス<span class="required">必須</span></dt>
+          <dd>
+            <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES); ?>">
+            <?php if($error['email'] == 'blank'): ?>
+            <p class="error">* メールアドレスを入力してください</p>
+            <?php endif; ?>
+            <?php if($error['email'] == 'duplicate'): ?>
+              <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
+              <?php endif; ?>
+          </dd>
+          <dt>パスワード<span class="required">必須</span></dt>
+          <dd>
+            <input type="password" name="password" size="10" maxlength="20" value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES); ?>">
+            <?php if($error['password'] == 'blank'): ?>
+            <p class="error">* パスワードを入力してください</p>
+            <?php endif; ?>
+            <?php if($error['password'] == 'length'): ?>
+            <p class="error">* パスワードは4文字以上で入力してください</p>
+            <?php endif; ?>
+          </dd>
+          <dt>写真など</dt>
+          <dd>
+            <input type="file" name="image" size="35">
+            <?php if($error['image'] == 'type'): ?>
+            <p class="error">* 写真などは「.gif」または「.jpg」の画像を指定してください</p>
+            <?php endif; ?>
+            <?php if(!empty($error)): ?>
+            <p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
+            <?php endif; ?>
+          </dd>
+        </dl>
+        <div><input type="submit" value="入力内容を確認する"></div>
+      </form>
+    </div>
 
-</div>
+  </div>
 </body>
 </html>
