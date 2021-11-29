3c7f0e5db987d684d72b883d0eee986ba1208fb0
akie27 <66990408+akie27@users.noreply.github.com>
2021-11-29 15:50:23 +0900

    エラーハンドリング

--- dbconnect.php
+++ dbconnect.php
@@ -5,9 +5,12 @@ try{
     host=localhost;
     charset=utf8',
     'root',
-    'root');
+    'root', 
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     
 }catch(PDOException $e){
     echo 'DB接続エラー: ' . $E->getMessage();
 }
+
+$db->query("SELECT wrongcolumn FROM wrongtable");
 ?>
