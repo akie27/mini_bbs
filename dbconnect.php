1914d7d81ffebfeac7d196eddb3d76bc1701c961
akie27 <66990408+akie27@users.noreply.github.com>
2021-11-29 13:45:13 +0900

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+try{
+    $db = new PDO('mysql:
+    dbname=mini_bbs;
+    host=localhost;
+    charset=utf8',
+    'root',
+    'root');
+    
+}catch(PDOException $e){
+    echo 'DB接続エラー: ' . $E->getMessage();
+}
+?>
